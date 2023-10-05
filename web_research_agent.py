40f715c975d01eb4e2967c57a5631b8f97626341
Jeremy Mizell <mizellj@ottercove.net>
2023-10-04 20:34:49 -0600

    feat: Enhance web research agent with webpage scraping and extraction
    - Add dependencies for web scraping: `beautifulsoup4`, `markdownify`, and `requests`
    - Introduce `scrape_and_extract`, a function that scrapes webpages and extracts relevant information
    - Refactor the `extract_webpage` function to optimize the extraction process and maintain response length
    - Implement retry mechanisms for more robust error handling in both `combine_results` and `web_search`
    - Refactor web search agent to generate search queries using GPT-3.5, improving the relevancy of search results
    - Improve result filtering by allowing agents to score results on a scale from 0 to 4, enhancing precision
    - Update documentation and add inline comments for clarity

--- web_research_agent.py
+++ web_research_agent.py
@@ -7,6 +7,9 @@ import openai
 import tiktoken
 from duckduckgo_search import DDGS
 import wikipedia
+import requests
+from bs4 import BeautifulSoup
+import markdownify
 
 
 MODEL_NAME = "gpt-3.5-turbo-16k-0613"
@@ -19,16 +22,136 @@ def token_count(string: str) -> int:
     return len(encoding.encode(string))
 
 
+def extract_webpage(question: str, url: str, response: str, page_markdown: str) -> str:
+    max_retry = 5
+    retry = 0
+
+    original_response_length = token_count(response)
+
+    while retry < max_retry:
+        msgs = [
+            {
+                "role": "system",
+                "content": f"""Your task is to extract relevant information as bullet points from this web page for the question: '{question}'.
+ 
+Instructions:
+1. Combine the information from the 'Current Response' and the 'New Page Segment'.
+2. Maintain or extend the length of the 'Current Response'; do not shorten it.
+3. Do not omit any details.
+4. Include quotes and relevant links.
+5. If the 'New Page Segment' does not offer any new information, return the 'Current Response' as is.
+
+Current Response:
+{response}
+
+New Page Segment:
+{page_markdown}
+
+Please proceed with the task."""
+            }
+        ]
+
+        new_response = call_api(msgs, stream=False)["choices"][0]["message"]["content"]
+
+        if token_count(new_response) >= original_response_length * 0.85:
+            return new_response
+
+        print(f"Retry {retry+1}: New response is shorter than original. Retrying for {url}")
+        retry += 1
+
+    print(f"Max retries reached. Returning original response. Failed segment for {url}")
+    return response 
+
+
+def scrape_and_extract(url: str, question: str) -> str:
+
+    markdown_text, status = scrape_page(url)
+    if status != "Success":
+        print(f"Scraping failed. Status: {status}")
+        return ""
+
+    current_response = ""    
+    segment = ""
+    segment_token_count = 0
+    count = 0
+    for word in markdown_text.split(" "):
+        tentative_segment = f"{segment} {word}"
+        tentative_token_count = token_count(tentative_segment)
+        if tentative_token_count <= MODEL_MAX_TOKENS * 0.5:
+            segment = tentative_segment
+            segment_token_count = tentative_token_count
+        else:
+            print(f"Processing page segment {count} for {url}")
+            current_response = extract_webpage(question, url, current_response, segment)
+            segment = word
+            segment_token_count = token_count(segment)
+            count=count+1
+    if segment:
+        print(f"Processing page segment {count} for {url}")
+        current_response = extract_webpage(question, url, current_response, segment)
+
+    return current_response
+
+
+def scrape_page(url: str) -> (str, str):
+    """
+    Scrape the content of a web page and return it as Markdown-formatted text.
+
+    Parameters:
+    - url (str): The URL of the web page to scrape.
+
+    Returns:
+    - str: The Markdown-formatted text content of the web page.
+    - str: Status message indicating the outcome of the operation ("Success" or an error message).
+
+    Exceptions:
+    - Raises requests.exceptions.RequestException for issues like connectivity, timeout, etc.
+
+    Example Usage:
+    >>> markdown_text, status = scrape_page('http://www.example.com')
+    >>> if status == "Success":
+    >>>     print(markdown_text)
+    >>> else:
+    >>>     print(f"Scraping failed. Status: {status}")
+    """
+
+    try:
+        response = requests.get(
+            url,
+            headers={
+                'User-Agent': "Mozilla/5.0 (Android 13; Mobile; rv:109.0) Gecko/118.0 Firefox/118.0"
+            },
+            timeout=20,
+        )
+        if response.status_code // 100 == 2:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            markdown_text = markdownify.markdownify(str(soup))
+            return markdown_text, "Success"
+        else:
+            print(f"Failed to fetch the webpage. Status Code: {response.status_code}")
+            return "", f"Failed. Status Code: {response.status_code}"
+    except requests.exceptions.RequestException as err:
+        print(f"An error occurred: {err}")
+        return "", f"An error occurred: {err}"
+
+
 def combine_results(question: str, results: str, new_results: str) -> str:
-    msgs = [
-        {
-            "role": "system",
-            "content": f"""Your task is to consolidate and summarize search results for the question: '{question}'.
+    max_retry = 5
+    retry = 0
+
+    original_response_length = token_count(results)
+
+    while retry < max_retry:
+        msgs = [
+            {
+                "role": "system",
+                "content": f"""Your task is to consolidate and summarize search results for the question: '{question}'.
 
 Instructions:
 1. Combine the information from the 'Current Result' and the 'New Result'.
 2. Do not omit any details from either result.
-3. If the 'New Result' does not offer any new information, return the 'Current Result' as is.
+3. Include links and quotes when relevant.
+4. If the 'New Result' does not offer any new information, return the 'Current Result' as is.
 
 Current Result:
 {results}
@@ -37,10 +160,20 @@ New Result:
 {new_results}
 
 Please proceed with the task."""
-        }
-    ]
-    response = call_api(msgs, stream=False)
-    return response["choices"][0]["message"]["content"]
+            }
+        ]
+
+        new_response = call_api(msgs, stream=False)["choices"][0]["message"]["content"]
+
+        if token_count(new_response) >= original_response_length * 0.85:
+            return new_response
+
+        print(f"Retry {retry+1}: New response is shorter than original. Retrying.")
+        retry += 1
+
+    print("Max retries reached. Returning original result.")
+    return results  # if max retries are reached, return the original result
+
 
 
 def threaded_filter(question, sliced_results, results, lock):
@@ -54,20 +187,24 @@ Title: {r['title']}
 URL: {r['href']}
 Content: {r['body']}
 
-If the result is relevant, answer with the answer YES, or NO only.
+Answer with an INTEGER score of the relevencey from 0 through 4. Reply only with 0, 1, 2, 3, or 4.
         """
             }
         ]
         response = call_api(msgs, stream=False)
-        match = str(response["choices"][0]["message"]
-                    ["content"]).lower() == "yes"
-        if match:
+        score = int(response["choices"][0]["message"]["content"])
+        print(f"Score: {score}, URL: {r['href']}")
+        if score == 4:
+            extracted_info = scrape_and_extract(r['href'], question)
+            if token_count(extracted_info) > token_count(r['body']):
+                r['body'] = extracted_info
+        if score >= 3:
             with lock:
                 results.append(r)
             print(f"Added {r['href']}")
 
 
-def combine(all_results: list, question: str, query: str, max_results: int = 250) -> str:
+def combine(all_results: list, question: str, query: str) -> str:
     results = []
     lock = threading.Lock()
 
@@ -90,7 +227,7 @@ def combine(all_results: list, question: str, query: str, max_results: int = 250
     combine_input = []
     for r in results:
         combine_input.append(r['body'])
-        if token_count("\n".join(combine_input)) > 1000:
+        if token_count("\n".join(combine_input)) > MODEL_MAX_TOKENS * 0.5:
             print("processing...")
             response = combine_results(
                 question, response, "\n".join(combine_input))
@@ -105,14 +242,22 @@ def combine(all_results: list, question: str, query: str, max_results: int = 250
 
 
 def web_search(question: str, query: str, max_results: int = 250) -> str:
-    all_results = []
-    with DDGS() as ddgs:
-        for r in ddgs.text(query, safesearch='off'):
-            all_results.append(r)
-            if len(all_results) >= max_results:
-                break
+    max_retry = 5
+    retry = 0
+    while retry < max_retry:
+        all_results = []
+        with DDGS() as ddgs:
+            for r in ddgs.text(query, safesearch='off'):
+                all_results.append(r)
+                if len(all_results) >= max_results:
+                    break
+        if len(all_results) > 0:
+            break
+        retry+=1
 
-    return combine(all_results, question, query, max_results)
+    print(f"Found {len(all_results)} web results to process")
+
+    return combine(all_results, question, query)
 
 
 def wikipedia_search(question: str, query: str, max_results: int = 250) -> str:
@@ -124,14 +269,15 @@ def wikipedia_search(question: str, query: str, max_results: int = 250) -> str:
             continue
 
         all_results.append({
-            "title":page_title,
+            "title": page_title,
             "href": wiki_page.url,
             "body": wiki_page.summary,
         })
         if len(all_results) >= max_results:
             break
+    print(f"Found {len(all_results)} wikipedia results to process")
 
-    return combine(all_results, question, query, max_results)
+    return combine(all_results, question, query)
 
 
 def call_api(msgs: list[dict], functions: list[dict] = None, stream: bool = True):
@@ -162,8 +308,27 @@ def call_api(msgs: list[dict], functions: list[dict] = None, stream: bool = True
 
 
 if __name__ == "__main__":
-    question = "Can you tell me something interesting about the denver capital?"
-    web_result = web_search(question, "denver capital")
-    wikipedia_result = wikipedia_search(question, "denver capital")
+    question = "I need a list of dispersed camping sites in Utah"
+    print(f"Question: {question}")
+    msgs = [
+        {
+            "role": "system",
+            "content": f"""Please output a web search query for this question: {question}"""
+        }
+    ]
+    response = call_api(msgs, stream=False)
+    search_query = response["choices"][0]["message"]["content"]
+    print(f"Search query: {search_query}")
+    web_result = web_search(question, search_query, max_results=100)
+    msgs = [
+        {
+            "role": "system",
+            "content": f"""Please output a wikipedia search query for this question: {question}"""
+        }
+    ]
+    response = call_api(msgs, stream=False)
+    search_query = response["choices"][0]["message"]["content"]
+    print(f"Search query: {search_query}")
+    wikipedia_result = wikipedia_search(question, search_query, max_results=100)
     print("processing....")
     print(combine_results(question, web_result, wikipedia_result))
